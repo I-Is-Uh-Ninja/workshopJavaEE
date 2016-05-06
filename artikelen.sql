28552ddc694d044980eb234b3a268a417b555215
Ian Nijhuis <iannijhuis@hotmail.com>
2016-05-06 14:15:06 +0200

    Voorbeeld artikelen sql

---
+++ artikelen.sql
@@ -0,0 +1,9 @@
+INSERT INTO `artikel`(`idArtikel`, `artikelnaam`, `artikelprijs`, `artikelnummer`, `artikelomschrijving`) VALUES (null,'Zonnebrandcrème',5.48,'0001','SPF-50');
+INSERT INTO `artikel`(`idArtikel`, `artikelnaam`, `artikelprijs`, `artikelnummer`, `artikelomschrijving`) VALUES (null,'Tandpasta',1.39,'0002','Coolmint');
+INSERT INTO `artikel`(`idArtikel`, `artikelnaam`, `artikelprijs`, `artikelnummer`, `artikelomschrijving`) VALUES (null,'Shampoo',3.47,'0003','Anti-roos shampoo');
+INSERT INTO `artikel`(`idArtikel`, `artikelnaam`, `artikelprijs`, `artikelnummer`, `artikelomschrijving`) VALUES (null,'Vitaminepillen',5.32,'0004','Pillen met vitamine-A, -B en -D');
+INSERT INTO `artikel`(`idArtikel`, `artikelnaam`, `artikelprijs`, `artikelnummer`, `artikelomschrijving`) VALUES (null,'Deodorant',2.99,'0005','Anti-transpirant');
+INSERT INTO `artikel`(`idArtikel`, `artikelnaam`, `artikelprijs`, `artikelnummer`, `artikelomschrijving`) VALUES (null,'Tandenborstel',2.15,'0006','Elektrische tandenborstel met Bluetooth');
+INSERT INTO `artikel`(`idArtikel`, `artikelnaam`, `artikelprijs`, `artikelnummer`, `artikelomschrijving`) VALUES (null,'Conditioner',3.14,'0007','Voor slap en futloos haar');
+INSERT INTO `artikel`(`idArtikel`, `artikelnaam`, `artikelprijs`, `artikelnummer`, `artikelomschrijving`) VALUES (null,'Scheermessen',6.98,'0008','Pak van 5 scheermessen met 3 messen');
+INSERT INTO `artikel`(`idArtikel`, `artikelnaam`, `artikelprijs`, `artikelnummer`, `artikelomschrijving`) VALUES (null,'Aftershave',4.95,'0009','Bijt niet');
