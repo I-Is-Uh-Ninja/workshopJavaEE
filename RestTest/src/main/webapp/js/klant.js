ddc7f392907ee192ec58a2edef295027d20c7a1f
Ian Nijhuis <iannijhuis@hotmail.com>
2016-06-09 16:43:26 +0200

    Klant validation toegevoegd
    jQuery validation, error messages gestyled

--- RestTest/src/main/webapp/js/klant.js
+++ RestTest/src/main/webapp/js/klant.js
@@ -18,6 +18,7 @@ $(document).ready(function(){
         });
     });*/
     
+    //get all klanten
     $.ajax({
         type: 'GET',
         url: restURL,
@@ -27,6 +28,7 @@ $(document).ready(function(){
         }
     });
     
+    //get klanten in function to be called somewhere else
     function getKlanten(){
         $.ajax({
             type: 'GET',
@@ -38,39 +40,49 @@ $(document).ready(function(){
         });
     }
     
+    //display a table of all klanten
     function displayKlantTable(klanten){
+        $("#klantenBody").empty();
         $.each(klanten, function(i, field){
-            $("#klantenBody").append("<tr id='" + field.idKlant + "'></tr>");
+            $("#klantenBody").append("<tr id='" + field.idKlant + "'></tr>"); //add row for each klant
             for (var p in field){
                 if(i===0){
-                    $("#klantenTitle").append("<th>" + p + "</th>");
+                    $("#klantenTitle").append("<th>" + p + "</th>"); //add header for every property
                 }
-                $("tr#" + field.idKlant).append("<td>" + field[p] + "</td>");
+                $("tr#" + field.idKlant).append("<td>" + field[p] + "</td>"); //add table cell for every value
             }
             if (i===0){
-                $("#klantenTitle").append("<th colspan='2'>Actie</th>");
+                $("#klantenTitle").append("<th colspan='2'>Actie</th>"); //add header for view- and delete-buttons
             }
-            $("tr#" + field.idKlant).append("<td id='view'><button type='button' id='"+ field.idKlant + "'>Bekijk klant</button>");
-            $("tr#" + field.idKlant).append("<td id='delete'><button type='button' id='"+ field.idKlant + "'>Verwijder klant</button>");
+            $("tr#" + field.idKlant).append("<td id='view'><button type='button' id='"+ field.idKlant + "'>Bekijk klant</button>"); //add view button
+            $("tr#" + field.idKlant).append("<td id='delete'><button type='button' id='"+ field.idKlant + "'>Verwijder klant</button>"); //add delete button
         });
     }
-
+    
+    //method for clicking the view-button -> go to viewKlant.html with klant ID as parameter
     $(document).on("click", "td#view button", function(){
         var id = {klantId : event.target.id};
         var idParam = $.param(id);
         location.href = "viewKlant.html?" + idParam; 
     });
     
+    //method for clicking the delete-button -> make ajax delete request for deleting klant
     $(document).on("click", "td#delete button", function(){
         var URL = restURL + "/" + event.target.id;
         ajaxDelete(URL);
     });
-
+    
+    //method for creating a new klant
     $("#nieuwKlant").submit(function(){
         //$.post(restURL, formToJson(), alert(formToJson()));
-        ajaxCreate(restURL);
+        //validate
+        if($("#nieuwKlant").valid()){ //calls the validate method
+            //now create
+            ajaxCreate(restURL);
+        }
     });
-
+    
+    //convert the input values to json string
     function formToJson(){
         return JSON.stringify({
             "achternaam": $("#achternaam").val(),
@@ -79,6 +91,46 @@ $(document).ready(function(){
             "voornaam": $("#voornaam").val()
         });
     }
+    
+    //validator for nieuwKlant form
+    $("#nieuwKlant").validate({
+        rules: { //add rules
+            voornaam:{
+                required: true,
+                maxlength: 50
+            },
+            tussenvoegsel:{
+                maxlength: 50
+            },
+            achternaam:{
+                required: true,
+                maxlength: 51
+            },
+            email: {
+                required: true,
+                email: true,
+                maxlength: 80
+            }
+        },
+        messages: {
+            voornaam: {
+                required: "Voornaam is verplicht",
+                maxlength: jQuery.validator.format("Voornaam mag maximaal {0} karakters lang zijn") //{0} is a callback to the maxlength value
+            },
+            tussenvoegsel: {
+                maxlength: jQuery.validator.format("Tussenvoegsel mag maximaal {0} karakters lang zijn")
+            },
+            achternaam: {
+                required: "Achternaam is verplicht",
+                maxlength: jQuery.validator.format("Achternaam mag maximaal {0} karakters lang zijn")
+            },
+            email: {
+                required: "E-mail is verplicht",
+                maxlength: jQuery.validator.format("E-mail mag maximaal {0} karakters lang zijn"),
+                email: "Geen geldig e-mail adres"
+            }
+        }
+    });
     
     //CRUD ajax calls
     function ajaxCreate(URL){
