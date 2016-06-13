68f0c125246dbad668af82d2965002669e7f48a9
Ian Nijhuis <iannijhuis@hotmail.com>
2016-06-13 16:49:45 +0200

    Artikel met bewerken werkend en validation
    Artikel table data ids zijn nu classes, ook dingen toegevoegd zodat de
    tabellen herladen op bewerken en toevoegen

--- RestTest/src/main/webapp/js/artikel.js
+++ RestTest/src/main/webapp/js/artikel.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
     var URL = "http://localhost:8080/RestTest/rest/artikel";
     
-    $.getJSON(URL, function(result){
-        $.each(result, function(i, field){
-            $("#artikelBody").append("<tr id='" + field.idArtikel + "'></tr>");
+    getArtikelen();
+    
+    function getArtikelen(){
+        $.getJSON(URL, function(result){
+            $("#artikelBody").empty();
+            $("#artikelTitle").empty();
+            $.each(result, function(i, field){
+                $("#artikelBody").append("<tr id='" + field.idArtikel + "'></tr>");
                 for (var p in field){
                     if(i===0){
                         $("#artikelTitle").append("<th>" + p + "</th>");
                     }
-                    $("tr#" + field.idArtikel).append("<td id='" + p + "'>" + field[p] + "</td>");
+                    $("tr#" + field.idArtikel).append("<td class='" + p + "'>" + field[p] + "</td>");
                 }
-            $("tr#" + field.idArtikel).append("<td id='edit'><button type='button' id='"+ field.idArtikel + "'>Bewerken</button>");
-            $("tr#" + field.idArtikel).append("<td id='delete'><button type='button' id='"+ field.idArtikel + "'>Verwijder artikel</button>");
+                $("tr#" + field.idArtikel).append("<td class='edit'><button type='button' id='"+ field.idArtikel + "'>Bewerken</button>");
+                $("tr#" + field.idArtikel).append("<td id='delete'><button type='button' id='"+ field.idArtikel + "'>Verwijder artikel</button>");
             });
         });
+    }
 
     /*$(document).on("click", "td#view button", function(){
         var currentId = {artikelId : event.target.id};
@@ -50,6 +56,7 @@ $(document).ready(function(){
             data: formToJson(),
             success: function(data, textStatus, jqXHR){
                 //alert(formToJson() + 'is toegevoegd.');
+                getArtikelen();
             },
             error: function(jqXHR, textStatus, errorThrown){
                 alert('Fout bij toevoegen artikel:' + "\n" + errorThrown + "\n" + formToJson());
@@ -72,46 +79,54 @@ $(document).ready(function(){
     
     //edit artikel
     var editArtikelClicked = false;
-    $(document).on("click", "td#edit button", function(event){
+    $(document).on("click", "td.edit button", function(event){
+        event.preventDefault();
         if(editArtikelClicked === false) {
-            var existingArtikelnaam = $("tr#" + event.target.id + " td#artikelnaam").text();
-            var existingArtikelnummer = $("tr#" + event.target.id + " td#artikelnummer").text();
-            var existingArtikelomschrijving = $("tr#" + event.target.id + " td#artikelomschrijving").text();
-            var existingArtikelprijs = $("tr#" + event.target.id + " td#artikelprijs").text();
+            var existingArtikelnaam = $("tr#" + event.target.id + " td.artikelnaam").text();
+            var existingArtikelnummer = $("tr#" + event.target.id + " td.artikelnummer").text();
+            var existingArtikelomschrijving = $("tr#" + event.target.id + " td.artikelomschrijving").text();
+            var existingArtikelprijs = $("tr#" + event.target.id + " td.artikelprijs").text();
             
-            $("tr#" + event.target.id + " td#artikelnaam").empty();
-            $("tr#" + event.target.id + " td#artikelnummer").empty();
-            $("tr#" + event.target.id + " td#artikelomschrijving").empty();
-            $("tr#" + event.target.id + " td#artikelprijs").empty();
+            $("tr#" + event.target.id + " td.artikelnaam").empty();
+            $("tr#" + event.target.id + " td.artikelnummer").empty();
+            $("tr#" + event.target.id + " td.artikelomschrijving").empty();
+            $("tr#" + event.target.id + " td.artikelprijs").empty();
             
-            $("tr#" + event.target.id + " td#artikelnaam").append("<input type='text' size=30 id='editArtikelnaam' value='" + existingArtikelnaam + "' /></td>");
-            $("tr#" + event.target.id + " td#artikelnummer").append("<input type='text' size=30 id='editArtikelnummer' value='" + existingArtikelnummer + "' /></td>");
-            $("tr#" + event.target.id + " td#artikelomschrijving").append("<input type='text' size=40 id='editArtikelomschrijving' value='" + existingArtikelomschrijving + "' /></td>");  
-            $("tr#" + event.target.id + " td#artikelprijs").append("<input type='text' size=6 id='editArtikelprijs' value='" + existingArtikelprijs + "' /></td>"); 
+            $("tr#" + event.target.id + " td.artikelnaam").append("<input type='text' size=30 name='artikelnaam' id='editArtikelnaam' value='" + existingArtikelnaam + "' /></td>");
+            $("tr#" + event.target.id + " td.artikelnummer").append("<input type='text' size=30 name='artikelnummer' id='editArtikelnummer' value='" + existingArtikelnummer + "' /></td>");
+            $("tr#" + event.target.id + " td.artikelomschrijving").append("<input type='text' size=40 name='artikelomschrijving' id='editArtikelomschrijving' value='" + existingArtikelomschrijving + "' /></td>");  
+            $("tr#" + event.target.id + " td.artikelprijs").append("<input type='text' size=6 name='artikelprijs' id='editArtikelprijs' value='" + existingArtikelprijs + "' /></td>"); 
             editArtikelClicked = true;
         } else {
-            var artikelJson = JSON.stringify({
-                "idArtikel": event.target.id,
-                "artikelnaam": $("input#editArtikelnaam").val(),
-                "artikelnummer": $("input#editArtikelnummer").val(),
-                "artikelomschrijving": $("input#editArtikelomschrijving").val(),
-                "artikelprijs": $("input#editArtikelprijs").val()
-            });
-            $.ajax({
-               type: 'PUT',
-               contentType: 'application/json',
-               url: URL + "/" + event.target.id,
-               data: artikelJson,
-               dataType: 'json',
-               success: function(data, textStatus, jqXHR){
-                    //alert("Artikel gewijzigd naar: " + artikelJson);
-               },
-               error: function(jqXHR, textStatus, errorThrown){
-                   //alert("Error: " + textStatus + "\n" + errorThrown + "\n" + artikelJson);
-               } 
-            });
-            editArtikelClicked = false;
-            //getArtikelen();
+            if($("form#alleArtikelen").valid()){
+                var artikelJson = JSON.stringify({
+                    "idArtikel": event.target.id,
+                    "artikelnaam": $("input#editArtikelnaam").val(),
+                    "artikelnummer": $("input#editArtikelnummer").val(),
+                    "artikelomschrijving": $("input#editArtikelomschrijving").val(),
+                    "artikelprijs": $("input#editArtikelprijs").val()
+                });
+                $.ajax({
+                   type: 'PUT',
+                   contentType: 'application/json',
+                   url: URL + "/" + event.target.id,
+                   data: artikelJson,
+                   dataType: 'json',
+                   success: function(data, textStatus, jqXHR){
+                        //alert("Artikel gewijzigd naar: " + artikelJson);
+                        
+                   },
+                   error: function(jqXHR, textStatus, errorThrown){
+                       //alert("Error: " + textStatus + "\n" + errorThrown + "\n" + artikelJson);
+                   },
+                   complete: function(){
+                        editArtikelClicked = false;
+                        getArtikelen();
+                   }
+                });
+                
+                //getArtikelen();
+            }
         }
     });
     //add regex method to validator
@@ -157,5 +172,45 @@ $(document).ready(function(){
                 maxlength: jQuery.validator.format("Artikelomschrijving mag maximaal {0} karakters lang zijn")
             }
         }
+    });
+    $("form#alleArtikelen").validate({
+        rules: { //add rules
+            artikelnaam:{
+                required: true,
+                maxlength: 80
+            },
+            artikelnummer:{
+                required: true,
+                maxlength: 45
+            },
+            artikelprijs:{
+                required: true,
+                maxlength: 6,
+                regex: /^\d{1,4}(\.{1}\d{1,2}){0,1}$/
+            },
+            artikelomschrijving: {
+                maxlength: 80
+            }
+        },
+        messages: {
+            artikelnaam: {
+                required: "Artikelnaam is verplicht",
+                maxlength: jQuery.validator.format("Artikelnaam mag maximaal {0} karakters lang zijn") //{0} is a callback to the maxlength value
+            },
+            artikelnummer: {
+                required: "Artikelnummer is verplicht",
+                maxlength: jQuery.validator.format("Artikelnummer mag maximaal {0} karakters lang zijn")
+            },
+            artikelprijs: {
+                required: "Artikelprijs is verplicht",
+                maxlength: jQuery.validator.format("Artikelprijs mag maximaal {0} nummers lang zijn"),
+                regex: "Mag alleen nummers bevatten, met 4 cijfers voor, en 2 cijfers na de komma"
+            },
+            artikelomschrijving: {
+                maxlength: jQuery.validator.format("Artikelomschrijving mag maximaal {0} karakters lang zijn")
+            }
+        },
+        errorElement : 'div',
+        errorLabelContainer: '.errorContainer'
     });
 });
